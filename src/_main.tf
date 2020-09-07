214d7d7118c601609a3462c3d449c609d15bf934
Samuel Veloso <veloso.lopez.samuel@gmail.com>
2020-09-07 14:44:33 +0200

    Upgrade terraform to 0.13

--- src/_main.tf
+++ src/_main.tf
@@ -1,23 +1,43 @@
 terraform {
+  required_version = ">= 0.13.0"
+
   backend "local" {}
+
+  required_providers {
+
+    libivrt = {
+      source  = "hashicorp/libvirt"
+      version = "~> 0.6.2"
+    }
+
+    ct = {
+      source  = "poseidon/ct"
+      version = "~> 0.6.1"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 2.1"
+    }
+
+    template = {
+      source  = "hashicorp/template"
+      version = "~> 2.1"
+    }
+
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 1.4"
+    }
+
+    random = {
+      source = "hashicorp/random"
+      version = "~> 2.3"
+    }
+
+  }
 }
 
 provider "libvirt" {
   uri = "qemu:///system"
-}
-
-provider "ct" {
-  # version = "~> 0.6.1"
-}
-
-provider "tls" {
-  version = "~> 2.1"
-}
-
-provider "template" {
-  version = "~> 2.1"
-}
-
-provider "local" {
-  version = "~> 1.4"
 }
